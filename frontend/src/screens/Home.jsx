619aaf25fd454fd19f5f8550cb471926cf3e703f
MaldingMalta <nischal.sha17@gmail.com>
2022-08-07 00:22:16 +0545

    Frontend Dummy Screens Added

---
+++ frontend/src/screens/Home.jsx
@@ -0,0 +1,52 @@
+import { Button, Typography, Box } from "@mui/material";
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <Typography
+        variant="h1"
+        noWrap
+        color="primary"
+        position="relative"
+        align="center"
+        sx={{
+          mt: 20,
+          fontWeight: "bold",
+        }}
+      >
+        PET ME
+      </Typography>
+      <Typography
+        variant="h6"
+        noWrap
+        color="blackv2"
+        position="relative"
+        align="center"
+      >
+        Save animals and help them find their home
+      </Typography>
+      <Box m={1} display="flex" justifyContent="center" alignItems="center">
+        <Button
+          variant="outlined"
+          sx={{
+            mt: 10,
+            borderRadius: "50px",
+          }}
+        >
+          <Typography
+            noWrap
+            color="blackv2"
+            position="relative"
+            align="center"
+            fontSize="24px"
+          >
+            View Animals
+          </Typography>
+        </Button>
+      </Box>
+    </>
+  );
+};
+
+export default Home;
