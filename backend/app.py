f13177cf5951b05613626fbc292ac9261e9fce9e
chalory <chantalrain.pino@gmail.com>
2022-08-07 02:11:20 +0800

    endpoint 1 fix

--- backend/app.py
+++ backend/app.py
@@ -41,8 +41,48 @@ def placeholder():
             '_petname': pet_name,
             '_pettype':pet_type,
             '_breed':breed,
+            '_sex': sex,
             '_description':description,
-            ' _medicaldescription':med_description
+             '_medicaldescription':med_description
+
+        }
+
+        insert_adoption_info(conn, entry)
+        
+        return "ok"
+
+    else:
+        all_adoptions = adoption_info(conn)
+        
+
+        print(all_adoptions)
+
+
+
+
+
+@app.route("/api/activity", methods=["GET", "POST"])
+@cross_origin()
+def placeholder():
+    if flask.request.method == "POST":
+        adoptor_name = flask.request.args.get('adoptor_name')
+        pet_id = flask.request.args.get('pet_id')
+        pet_name = flask.request.args.get('pet_name')
+        pet_type = flask.request.args.get('pet_type')
+        breed = flask.request.args.get('breed')
+        sex = flask.request.args.get('sex')
+        description = flask.request.args.get('description')
+        med_description = flask.request.args.get('med_description')
+
+        entry = {
+            'adoptorname': adoptor_name,
+            '_petid':pet_id,
+            '_petname': pet_name,
+            '_pettype':pet_type,
+            '_breed':breed,
+            '_sex': sex,
+            '_description':description,
+             '_medicaldescription':med_description
 
         }
 
--- backend/db.py
+++ backend/db.py
@@ -8,9 +8,9 @@ conn = psycopg2.connect("postgresql://chantal:kuwhacks2022@free-tier6.gcp-asia-s
 def info(conn):
     with conn.cursor() as cur:
         cur.execute(
-             "CREATE TABLE IF NOT EXISTS adopter_info (name VARCHAR(15), address VARCHAR(300), animal VARCHAR(50), id INT)"),
+             "CREATE TABLE IF NOT EXISTS adopter_info (name VARCHAR(15), address VARCHAR(300), email VARCHAR(50), mobile_number VARCHAR(50), animal_id INT)"),
         cur.execute(
-             "CREATE TABLE IF NOT EXISTS adoption_info (adoptorName VARCHAR(60),_petId INT,_petName VARCHAR(60),_petType VARCHAR(60),_breed VARCHAR(60),_sex VARCHAR(60),_description VARCHAR(300),_medicalDescription VARCHAR(300))")
+             "CREATE TABLE IF NOT EXISTS adoption_info (adoptorName VARCHAR(60),_petId INT,_petName VARCHAR(60),_petType VARCHAR(60),_breed VARCHAR(60),_sex VARCHAR(60),_description VARCHAR(300),_medicalDescription VARCHAR(300), image_hash VARCHAR(600))")
     
       
       
@@ -25,7 +25,7 @@ info(conn)
 def insert_adoption_info(conn, entry):
      with conn.cursor() as cur:
         cur.execute(
-           f"UPSERT INTO adoption_info (adoptorname, _petid, _petname, _pettype, _breed,  _sex, _description, _medicaldescription) VALUES ('{entry['adoptorname']}','{entry['_petid']}', '{entry[' _petname']}', '{entry['_pettype']}', '{entry['_breed']}', '{entry['_sex']}', '{entry['_description']}', '{entry['_medicaldescription']}')"
+           f"UPSERT INTO adoption_info (adoptorname, _petid, _petname, _pettype, _breed,  _sex, _description, _medicaldescription) VALUES ('{entry['adoptorname']}','{entry['_petid']}', '{entry[' _petname']}', '{entry['_pettype']}', '{entry['_breed']}', '{entry['_sex']}', '{entry['_description']}', '{entry['_medicaldescription']}', {entry['image_hash']}')"
           )
        
       
@@ -46,6 +46,20 @@ def adoption_info(conn):
         logging.debug("create_accounts(): status message: %s",
                       cur.statusmessage)
 
+
+
+def insert_adoption_info(conn, entry):
+     with conn.cursor() as cur:
+        cur.execute(
+           f"UPSERT INTO adoption_info (adoptorname, _petid, _petname, _pettype, _breed,  _sex, _description, _medicaldescription) VALUES ('{entry['adoptorname']}','{entry['_petid']}', '{entry[' _petname']}', '{entry['_pettype']}', '{entry['_breed']}', '{entry['_sex']}', '{entry['_description']}', '{entry['_medicaldescription']}', {entry['image_hash']}')"
+          )
+       
+      
+      
+        logging.debug("create_accounts(): status message: %s",
+                      cur.statusmessage)
+
+     conn.commit()
      conn.commit()
 
 
